b2068aa409a2ae0bf9e5d34b5deb9721f2a9a836
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-07-27 12:43:08 +0900

    feat: ownerOf
    - hardcode contract address temporally.
    - lint fix.

--- simpleNFT/packages/frontend/src/App.js
+++ simpleNFT/packages/frontend/src/App.js
@@ -6,7 +6,7 @@ import {abi,bytecode} from "../../contracts/artifacts/contracts/NFT.sol/NFT.json
 
 //Providerを作成。Ethereumへのネットワーク接続を管理します。
 const provider = new ethers.providers.Web3Provider(window.ethereum);
-let address = "" //NFTのdeploy時のアドレスを指定する。
+let address = "0xDc25d7D42164345B0D6b94AFE673cD1692C9D1A1" //NFTのdeploy時のアドレスを指定する。
 //Clickの処理.NFTをDeploy
 const buttonDeploy = async() => {
   //署名を取得します。コントラクトの作成には署名が必要です。基本的に、コントラクトの生成や値が変わる処理にはマイニングが必要なので署名が必要です。
@@ -34,7 +34,7 @@ const buttonMint = async() => {
   //以前にdeployしたコントラクトのアドレスからコントラクトを特定します。
   const contract = new ethers.Contract(address, abi, provider);
   //署名を付けてコントラクトのmintを実行します。
-  const {hash } = await contract.connect(signer).mint(signer.getAddress());//metamaskの署名を要求する
+  const { hash } = await contract.connect(signer).mint(signer.getAddress());//metamaskの署名を要求する
   console.log(contract);
   console.log(address);
   //rinkebyのchainidのときはetherscanのリンクを表示
@@ -53,6 +53,12 @@ const buttonBalanceOf = async() => {
   const contract = new ethers.Contract(address, abi, provider);
   console.log(await contract.connect(signer).balanceOf(signer.getAddress()));
 }
+//Clickの処理.NFTのownerOf
+const buttonOwnerOf = async() => {
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(address, abi, provider);
+  console.log(await contract.connect(signer).ownerOf(0));
+}
 function App() {
   return (
     <div className="App">
@@ -62,6 +68,7 @@ function App() {
     <button id="test2" onClick={buttonMint}>NFT mint</button><br/>
     <button id="test2" onClick={buttonSupply}>NFT totalSupply</button><br />
     <button id="test3" onClick={buttonBalanceOf}>NFT balanceOf</button><br />
+    <button id="test4" onClick={buttonOwnerOf}>NFT ownerOf</button><br />
     </div>
   );
 }
